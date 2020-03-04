26e623f71ad2b994eb7f3d913ab5e0d673c202be
Edward Jacobs <eddielawrence23@gmail.com>
2020-03-03 20:21:01 -0600

    FEAT: bookings controller

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,29 @@
+class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+    @island = Island.find(params[:island_id])
+  end
+
+  def create
+    @booking = Booking.new
+    @island = Island.find(params[:island_id])
+    @booking.user = current_user
+    @booking.island = @island
+
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
+end
