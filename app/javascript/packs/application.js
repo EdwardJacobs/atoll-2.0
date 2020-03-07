0714b624dd764c45b48500b179ce6057747601da
Edward Jacobs <eddielawrence23@gmail.com>
2020-03-06 20:58:59 -0600

    FIX: try to fix signout

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,3 @@
-// require("@rails/ujs").start()
-// require("@rails/activestorage").start()
-// require("channels")
-
 import "bootstrap"
 
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
